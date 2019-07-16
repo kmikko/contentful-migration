7fc7b8b4acd31062cbe8f99f2d5d0c6f6735c0fb
Justin Powell <jpowellcs@gmail.com>
2019-07-16 17:07:47 -0500

    use fetchAllPaginatedItems to get history
    we ran into an issue in a project where the default API entries limit
    (100) was lower than our migration count, and thus we were attempting to
    re-run migrations. This should fix this since the fetch method walks
    through the paginated results and concats them into one array of items.

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -124,6 +124,31 @@ export default class Fetcher implements APIFetcher {
     })
   }
 
+  async getMigrationHistory (): Promise<MigrationHistory[]> {
+
+    try {
+      const items = await this.fetchAllPaginatedItems<APIEntry>(
+        '/entries',
+        { content_type: 'migrationHistory' }
+      )
+
+      return items.map(item => new MigrationHistory(item.sys.id, item.fields as any))
+    } catch (e) {
+      if (e.name !== 'InvalidQuery') {
+        throw(e)
+      }
+      const msg = JSON.parse(e.message)
+
+      const errors = msg.details && msg.details.errors
+      if (errors && errors.length > 0 && errors[0].name === 'unknownContentType') {
+        // No migration history
+        return []
+      } else {
+        throw(e)
+      }
+    }
+  }
+
   private async fetchEditorInterface (id: string, editorInterfaces: Map<string, APIEditorInterfaces>) {
     try {
       const response = await this.makeRequest({
@@ -142,30 +167,6 @@ export default class Fetcher implements APIFetcher {
         })
       } else {
         throw error
-      }
-    }
-  }
-
-  async getMigrationHistory (): Promise<MigrationHistory[]> {
-
-    try {
-      const response = await this.makeRequest({
-        method: 'GET',
-        url: '/entries?content_type=migrationHistory'
-      })
-      return response.items.map(item => new MigrationHistory(item.sys.id, item.fields))
-    }catch(e) {
-      if (e.name != 'InvalidQuery') {
-        throw(e)
-      }
-      const msg = JSON.parse(e.message)
-
-      const errors = msg.details && msg.details.errors
-      if (errors && errors.length > 0 && errors[0].name == 'unknownContentType') {
-        // No migration history
-        return []
-      } else {
-        throw(e)
       }
     }
   }
